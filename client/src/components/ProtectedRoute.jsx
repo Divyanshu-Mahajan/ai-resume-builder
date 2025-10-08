968204f2df6070d7801d5c8969500fabfbd2efa1
Divyanshu Mahajan <divyanshumahajan9191@gmail.com>
2025-10-08 17:23:43 +0530

    fix: add loading state handling in ProtectedRoute component

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import Card from "../UI/Card";
 
 const ProtectedRoute = ({ children }) => {
-    const { isAuthenticated } = useSelector((state) => state.auth);
+    const { isAuthenticated, loading } = useSelector((state) => state.auth);
+
+    if(loading){
+        return(
+            <Card>
+                Loading...
+            </Card>
+        )
+    }
 
     if (!isAuthenticated) {
         return <Navigate to="/login" replace />;
